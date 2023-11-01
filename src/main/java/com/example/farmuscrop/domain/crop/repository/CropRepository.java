93acecd61d4724f46347f340a7b92c6c9acb32ba
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-02 04:08:36 +0900

    [feat] 작물 Create 서비스 + saveCrop + createCrop + checkCropPresent

--- src/main/java/com/example/farmuscrop/domain/crop/repository/CropRepository.java
+++ src/main/java/com/example/farmuscrop/domain/crop/repository/CropRepository.java
@@ -1,7 +1,12 @@
 package com.example.farmuscrop.domain.crop.repository;
 
+import com.example.farmuscrop.domain.crop.document.Crop;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CropRepository extends MongoRepository<CropRepository, String> {
+import java.util.Optional;
+
+public interface CropRepository extends MongoRepository<Crop, String> {
+
+    Optional<Crop> findByName(String name);
 }
--- src/main/java/com/example/farmuscrop/domain/crop/service/CropService.java
+++ src/main/java/com/example/farmuscrop/domain/crop/service/CropService.java
@@ -1,12 +1,47 @@
 package com.example.farmuscrop.domain.crop.service;
 
+import com.example.farmuscrop.domain.crop.document.Crop;
+import com.example.farmuscrop.domain.crop.dto.req.CreateCropRequestDto;
+import com.example.farmuscrop.domain.crop.dto.res.CreateCropResponseDto;
 import com.example.farmuscrop.domain.crop.repository.CropRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CropService {
 
     private final CropRepository cropRepository;
+    private final MongoTemplate mongoTemplate;
+
+//    public Crop.Step findStepByNum(int num) {
+//        return mongoTemplate.findById(num, Crop.Step.class);
+//    }
+//
+//    public List<Crop.Step> findAllSteps() {
+//        return mongoTemplate.findAll(Crop.Step.class);
+//    }
+
+    public CreateCropResponseDto saveCrop(CreateCropRequestDto requestDto) {
+        Crop crop = createCrop(requestDto);
+        cropRepository.save(crop);
+
+        return CreateCropResponseDto.of(crop.getId());
+    }
+
+    public Crop createCrop(CreateCropRequestDto requestDto) {
+        return Crop.createCrop(
+                requestDto.getName(),
+                requestDto.getDifficulty(),
+                requestDto.getSteps(),
+                requestDto.getImageUrl()
+        );
+    }
+
+    public Boolean checkCropPresent(String name) {
+        return cropRepository.findByName(name).isPresent();
+    }
 }
