d6d14f8e4de0f9b1e65c4f9d48991002de07691b
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-02 04:06:36 +0900

    [feat] 작물 추가 요청, 응답 dto

---
+++ src/main/java/com/example/farmuscrop/domain/crop/dto/req/CreateCropRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.farmuscrop.domain.crop.dto.req;
+
+import com.example.farmuscrop.domain.crop.document.Crop;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateCropRequestDto {
+
+    private String name;
+    private String difficulty;
+    private String imageUrl;
+    private List<Crop.Step> steps;
+}
