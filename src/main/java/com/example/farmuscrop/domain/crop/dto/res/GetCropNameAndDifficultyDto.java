837c85e073cf8908fe3cf98237c1122dceb58fb7
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-02 04:26:24 +0900

    [feat] 작물 이름 난이도 조회 응답 dto

---
+++ src/main/java/com/example/farmuscrop/domain/crop/dto/res/GetCropNameAndDifficultyDto.java
@@ -0,0 +1,14 @@
+package com.example.farmuscrop.domain.crop.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@Getter
+public class GetCropNameAndDifficultyDto {
+
+    private String name;
+    private String difficulty;
+}
