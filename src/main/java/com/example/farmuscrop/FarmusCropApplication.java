d1afd7a657ce2cfbeb549401c98967615b926ec0
MinchoGreenT <dldmstjq99@gmail.com>
2023-11-01 22:45:36 +0900

    initial commit

---
+++ src/main/java/com/example/farmuscrop/FarmusCropApplication.java
@@ -0,0 +1,15 @@
+package com.example.farmuscrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // eureka client
+public class FarmusCropApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmusCropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmuscrop/FarmusCropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmuscrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmusCropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
